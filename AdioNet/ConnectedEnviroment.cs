57f4a95564077f66e75754f780d4df241adf4d74
Kumar <jitendra.i.kumar@capgemini.com>
2024-11-27 15:39:30 +0530

    Initial server commit

---
+++ AdioNet/ConnectedEnviroment.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace AdioNet
+{
+    public class ConnectedEnviroment
+    {
+        SqlConnection connection;
+        SqlCommand cmd;
+
+        string conString = "Data Source=LIN-5CG4372MXS\\SQLEXPRESS;Integrated Security=True;Initial Catalog=EmpDb";
+        public void AddData(int a  , int b) {
+
+            connection = new SqlConnection(conString);
+            string sql = $"insert into Temptable values ({a} , {b})";
+            connection.Open();
+            cmd = new SqlCommand(sql, connection);
+            int count = cmd.ExecuteNonQuery(); 
+            if (count > 0)
+            {
+                Console.WriteLine("Data inserted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Data insertion failed");
+            }
+            connection.Close();
+        }//insert
+        public void ModifyData(int a , int b) {
+        connection = new SqlConnection(conString);
+            string sql = $"update TempTable set did = {b} where ID ={a} ";
+            connection.Open();
+            cmd = new SqlCommand(sql, connection);
+            int count = cmd.ExecuteNonQuery();
+            if (count > 0)
+            {
+                Console.WriteLine("Data updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Data updation failed");
+            }
+            connection.Close();
+        }
+        //update
+        public void DeleteData(int id) { 
+                connection = new SqlConnection(conString);
+            string sql = $"delete from TempTable where Id = {id}";
+           
+            // show data before deletion
+            Console.WriteLine("deleted the following record");
+            this.ReadDataById(id);
+            connection.Open();
+
+
+            cmd = new SqlCommand(sql, connection);
+            int count = cmd.ExecuteNonQuery();
+            if (count > 0)
+            {
+                Console.WriteLine("Data deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Data deletion failed");
+            }
+            connection.Close();
+        }//delete
+        public void ReadDataById(int a) { 
+            connection = new SqlConnection(conString);
+            string sql = $"select * from TempTable where ID = {a}";
+            connection.Open();
+            cmd = new SqlCommand(sql, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            Console.WriteLine("Displaying All records\nID\tValue\n");
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i] + "\t");
+                }
+                Console.WriteLine();
+            }
+            connection.Close();
+        }
+        public void ReadData() { 
+           
+
+        connection = new SqlConnection(conString);
+            string sql = "select * from TempTable";
+            connection.Open();
+            cmd = new SqlCommand(sql, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            Console.WriteLine("Displaying All records\nID\tValue\n");
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write(reader[i] + "\t");
+                }
+                Console.WriteLine();
+            }
+            connection.Close();
+
+        }//select
+    }
+}
---
+++ AdioNet/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AdioNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. read all data\n2. read data by ID\n3. insert new data\n4. Modify Data\n5. Delete data by id\n6. Exit ");
+            
+            ConnectedEnviroment connectedEnviroment = new ConnectedEnviroment();
+            
+            while (true)
+            {
+                Console.WriteLine("Enter your choice: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        connectedEnviroment.ReadData();
+                        break;
+                    case 2:
+                        Console.WriteLine("enter id of record");
+                        int id = Convert.ToInt32(Console.ReadLine());
+                        connectedEnviroment.ReadDataById(id);
+                        break;
+                    case 3:
+                        int idForInsert, ValueForId;
+                        Console.WriteLine("Enter ID for new record: ");
+                        idForInsert = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter value for new record: ");
+                        ValueForId = Convert.ToInt32(Console.ReadLine());
+                        connectedEnviroment.AddData(idForInsert, ValueForId);
+
+                        break;
+                    case 4:
+                        int idForUpdate, ValueForIdUpdate;
+                        Console.WriteLine("Enter ID for record to update: ");
+                        idForUpdate = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter value for record to update: ");
+                        ValueForIdUpdate = Convert.ToInt32(Console.ReadLine());
+                        connectedEnviroment.ModifyData(idForUpdate, ValueForIdUpdate);
+                        break;
+                    case 5:
+                        int idForDeletion;
+                        Console.WriteLine("Enter ID for record to delete: ");
+                        idForDeletion = Convert.ToInt32(Console.ReadLine());
+                        connectedEnviroment.DeleteData(idForDeletion);
+                        break;
+                    case 6:
+                        Console.WriteLine("Thank you for using the app");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+            
+            //Console.ReadKey();
+        }
+    }
+}
